48193b5f018cb099ca722b0516c7213a45f4db10
AlbertoEng <jesyrom0825@gmail.com>
2023-09-12 22:34:28 -0600

    corrigiendo redireccion

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,30 +1,35 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const CreatePost = () => {
 
-    const { register, handleSubmit , errors} = useForm();
-    const savePost = async ( data )=> await fetch('https://mi-proyecto-7bd3d-default-rtdb.firebaseio.com/postv2/.json',{
-        method: 'post',
-        body: JSON.stringify(data)
-    })
+    const { register, handleSubmit, errors } = useForm();
+    const navigate = useNavigate();
+    const savePost = async (data) => {
+        const resp = await fetch('https://mi-proyecto-7bd3d-default-rtdb.firebaseio.com/postv2/.json', {
+            method: 'post',
+            body: JSON.stringify(data)
+        })
+        navigate('/');
+    }
 
     return (
         <>
             <div className="container">
                 <h1 className='text-center mt-5'>Crear Post</h1>
                 <div className="row mt-5 d-flex justify-content-center">
-                    <div className="col-12" style={{ maxWidth: '600px'}}>
+                    <div className="col-12" style={{ maxWidth: '600px' }}>
                         <form className='form-group bg-dark p-5 rounded' onSubmit={handleSubmit(savePost)}>
                             {/* register your input into the hook by invoking the "register" function */}
-                            <input id='title'  placeholder='Titulo del Post' className='form-control mt-3' defaultValue="" {...register("title")} />
-                            <input id='content'  placeholder='Contenido del Post' className='form-control mt-3' defaultValue="" {...register("content")} />
-                            <input id='cover'  placeholder='Url de la imagen' className='form-control mt-3' defaultValue="" {...register("cover")} />
-                            <input id='autor'  placeholder='Autor' className='form-control mt-3' defaultValue="" {...register("autor")} />
-                            <input id='tags'  placeholder='#reactNative #flutter' className='form-control mt-3' defaultValue="" {...register("tags")} />
+                            <input id='title' placeholder='Titulo del Post' className='form-control mt-3' defaultValue="" {...register("title")} />
+                            <input id='content' placeholder='Contenido del Post' className='form-control mt-3' defaultValue="" {...register("content")} />
+                            <input id='cover' placeholder='Url de la imagen' className='form-control mt-3' defaultValue="" {...register("cover")} />
+                            <input id='autor' placeholder='Autor' className='form-control mt-3' defaultValue="" {...register("autor")} />
+                            <input id='tags' placeholder='#reactNative #flutter' className='form-control mt-3' defaultValue="" {...register("tags")} />
                             {/* {errors.exampleRequired && <span>This field is required</span>} */}
-                            <Link to='/'><button type="submit"  className='btn btn-primary mt-3'>Enviar</button></Link> 
+                            <button  type="submit"  className='btn btn-primary mt-3' >Enviar</button>
                         </form>
                     </div>
                 </div>
